817d97cf25ef14cfb534b9a8539c0821b4407b80
krishnaprasath <prasathkrishna002@gmail.com>
2026-04-28 10:50:13 +0530

    kk

--- logsselenium/src/test/java/logcreation/BaseTest.java
+++ logsselenium/src/test/java/logcreation/BaseTest.java
@@ -4,12 +4,12 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
 
-    protected WebDriver d;
+    protected static WebDriver d;
 
     @BeforeMethod
     public void beforeTest() {
@@ -19,7 +19,7 @@ public class BaseTest {
         d.get("https://tutorialsninja.com/demo/index.php");
     }
 
-    @AfterMethod
+    @AfterClass
     public void afterTest() {
         if (d != null) {
             d.quit();
